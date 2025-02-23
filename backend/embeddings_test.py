5dd6d71771993b35e3f1ac8c79c512794fb63ffe
crunchydosa123 <prathamgadkari@gmail.com>
2025-02-23 17:50:40 +0530

    minor changes

--- backend/embeddings_test.py
+++ backend/embeddings_test.py
@@ -7,7 +7,7 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 sentences = [
     "The weather is lovely today.",
     "It's so sunny outside!",
-    "He drove to the stadium.",
+    "He ran a marathon.",
 ]
 
 # 2. Calculate embeddings by calling model.encode()
